52fc623853818ead0f37d30efd969cc535656ddd
qilingzon <qilingzon@qq.com>
2026-01-14 21:58:01 +0800

    2

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,17 @@
 export default {
   plugins: {
     '@tailwindcss/postcss': {},
-    autoprefixer: {},
+    autoprefixer: {
+      // 兼容更多浏览器
+      overrideBrowserslist: [
+        '> 1%',
+        'last 2 versions',
+        'Chrome >= 60',
+        'Firefox >= 60',
+        'Safari >= 12',
+        'iOS >= 12',
+        'Android >= 5'
+      ]
+    },
   },
 }
